db50ee8aa3c6c372b48cc8e422c305a57e01171c
kristamcmanus <k.mcmanus310@gmail.com>
2021-12-07 12:22:19 -0500

    Enable first 10 people to display on page

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,34 @@
-// URL to retrieve people data
-let peopleUrl = "https://swapi.dev/api/people/1";
-let person = document.getElementById("person-info");
+const results = document.querySelector('#results');
 
-fetch(peopleUrl).then(response => response.json())
-    .then(json => {
-        console.log(json)
-        person.innerHTML = `<h5>${json.name}<h5>`
-    })
+async function asyncFetch(value) {
+    const res = await fetch(`https://swapi.dev/api/${value}/`);
+    const data = await res.json();
+    displayResults(data, value)
+}
+
+function displayResults(data, value) {
+    let output = "";
+    console.log(data);
+    if (value === 'people') {
+        data.results.forEach(item => {
+            output += `
+            <div class="card p-3 m-3" style="width: 16rem">
+                <h4 class="card-title text-center">${item.name}</h4>
+                <div class="card-content">
+                    <span>Birth Year</span>: ${item.birth_year}<br> 
+                    <span>Height</span>: ${item.height}<br>
+                    <span>Mass</span>: ${item.mass}<br>
+                    <span style="text-transform:capitalize">Eye Color</span>: ${item.eye_color}<br>
+                    <span>Number of Films</span>: ${item.films.length}<br>
+                </div>
+            </div>    
+            `
+        })
+    }
+    results.innerHTML = output;
+}
+
+document.querySelector('#buttons').addEventListener('click', e => {
+    asyncFetch(e.target.textContent.trim().toLowerCase());
+})
+
